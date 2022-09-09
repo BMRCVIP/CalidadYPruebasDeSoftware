ffe2a59bdea64f4771340b36290fe25eee63911f
Brandon Rodriguez <89100676+BMRCVIP@users.noreply.github.com>
2022-09-09 08:31:30 -0500

    Actualización de la T1

---
+++ CalidadT1/CalidadT1/CalidadT1/RomanoGenerator.cs
@@ -0,0 +1,40 @@
+namespace CalidadT1;
+public class RomanoGenerator
+{
+    private Dictionary<int, string> romanos = new Dictionary<int, string>()
+    {
+        //añadir el 1000
+        { 1000, "M"},
+        { 500, "D" },
+        { 100, "C" },
+        { 50, "L" },
+        { 49, "XLIX"},
+        { 40, "XL"},
+        { 10, "X" },
+        { 9, "IX"},
+        { 5, "V" },
+        //agregar el 4 al diccionario
+        { 4, "IV"},
+        { 1, "I" },
+    };
+    // si se envia un numero menor a 1 debe enviar error/ validar hasta el 1000
+    public string GetRomano(int number)
+    {
+        if (number <= 0)
+            return "error";
+        if (number >= 1000)
+            return "numero mayor a mil";
+        var romano = "";
+            while (number > 0)
+            {
+                foreach (KeyValuePair<int, string> entry in romanos)
+                {
+                    if (number < entry.Key) continue;
+                    romano += entry.Value;
+                    number -= entry.Key;
+                    break;
+                }
+            }
+            return romano;
+    }
+}
---
+++ CalidadT1/CalidadT1/TestCalidadT1/PUnumeros.cs
@@ -0,0 +1,181 @@
+﻿using CalidadT1;
+
+namespace TestCalidadT1;
+public class PUnumeros
+{
+
+    [Test]
+    public void PruebaValidacion_1()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(750);
+        Assert.AreEqual("DCCL", validar);
+    }
+    [Test]
+    public void PruebaValidacion_2()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(58);
+        Assert.AreEqual("LVIII", validar);
+    }
+    [Test]
+    public void PruebaValidacion_3()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(452);
+        Assert.AreEqual("CCCCLII", validar);
+    }
+    [Test]
+    public void PruebaValidacion_4()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(674);
+        Assert.AreEqual("DCCL", validar);
+        
+    }
+    [Test]
+    public void PruebaValidacion_5()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(346);
+        Assert.AreEqual("CCCXXXXVI", validar);
+        
+    }
+    [Test]
+    public void PruebaValidacion_6()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(731);
+        Assert.AreEqual("DCCXXXI", validar);
+        
+    }
+    [Test]
+    public void PruebaValidacion_7()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(750);
+        Assert.AreEqual("DCCL", validar);
+    }
+    [Test]
+    public void PruebaValidacion_8()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(423);
+        Assert.AreEqual("CCCCXXIII", validar);
+    }
+    [Test]
+    public void PruebaValidacion_9()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(750);
+        Assert.AreEqual("DCCL", validar);
+        
+    }
+    [Test]
+    public void PruebaValidacion_10()
+    {
+        //XCV
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(95);
+        Assert.AreEqual("LXXXXV", validar);
+    }
+   
+    [Test]
+    public void PruebaValidacion_11()
+    {
+        //validar que stá mal
+        //CCXXXIV
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(234);
+        Assert.AreEqual("CCXXXIIII", validar);
+        
+    }
+    [Test]
+    public void PruebaValidacion_12()
+    {
+        //VALIDAR
+        //DCXXXXI
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(641);
+        Assert.AreEqual("DCXLI", validar);
+        }
+    [Test]
+    public void PruebaValidacion_13()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(4000);
+        Assert.AreEqual("numero mayor a mil", validar);
+    }
+    [Test]
+    public void PruebaValidacion_14()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(-423);
+        Assert.AreEqual("error", validar);
+    }
+    [Test]
+    public void PruebaValidacion_15()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(1000);
+        Assert.AreEqual("numero mayor a mil", validar);
+    }
+    [Test]
+    public void PruebaValidacion_16()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(-5232432);
+        Assert.AreEqual("error", validar);
+    }
+    [Test]
+    public void PruebaValidacion_17()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(42334);
+        Assert.AreEqual("numero mayor a mil", validar);
+    }
+
+    [Test]
+    public void PruebaValidacion_18()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(-4);
+        Assert.AreEqual("error", validar);
+    }
+
+    [Test]
+    public void PruebaValidacion_19()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(1);
+        Assert.AreEqual("I", validar);
+    }
+    [Test]
+    public void PruebaValidacion_20()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(2);
+        Assert.AreEqual("II", validar);
+    }
+    [Test]
+    public void PruebaValidacion_21()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(3);
+        Assert.AreEqual("III", validar);
+    }
+    [Test]
+    public void PruebaValidacion_22()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(4);
+        Assert.AreEqual("IV", validar);
+    }
+    [Test]
+    public void PruebaValidacion_23()
+    {
+        var numerosRomanos = new RomanoGenerator();
+        var validar = numerosRomanos.GetRomano(10);
+        Assert.AreEqual("X", validar);
+    }
+}
